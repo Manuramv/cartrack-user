ca6d10117118868f900f760d3f0fdd7ae0d5bb54
Manuv <manuram692@gmail.com>
2020-08-01 22:48:28 +0800

    setup user and user details screen

--- app/src/main/java/com/cartrack/users/ui/home/HomeActivity.kt
+++ app/src/main/java/com/cartrack/users/ui/home/HomeActivity.kt
@@ -2,12 +2,29 @@ package com.cartrack.users.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.cartrack.users.R
+import com.cartrack.users.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val binding: ActivityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        /*val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)*/
+
+
     }
 }
--- app/src/main/java/com/cartrack/users/ui/login/LoginActivity.kt
+++ app/src/main/java/com/cartrack/users/ui/login/LoginActivity.kt
@@ -56,7 +56,6 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-
     private fun navigateToHomeScreen(){
         startActivity(Intent(this, HomeActivity::class.java))
     }
