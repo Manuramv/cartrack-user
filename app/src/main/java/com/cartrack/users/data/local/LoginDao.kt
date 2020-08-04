49c1a6fc045e6ed223954bb54380ac749d31067b
Manuv <manuram692@gmail.com>
2020-08-05 02:20:51 +0800

    Making email field case sensitive

--- app/src/main/java/com/cartrack/users/data/local/LoginDao.kt
+++ app/src/main/java/com/cartrack/users/data/local/LoginDao.kt
@@ -15,10 +15,10 @@ interface LoginDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(user : List<User>): List<Long>
 
-    @Query("SELECT * FROM USERTABLE WHERE userName =:userName and password= :password")
-    fun getUser(userName: String, password:String ) :LiveData<User>
+    @Query("SELECT * FROM USERTABLE WHERE userName =:userName COLLATE NOCASE and password= :password")
+    fun getUser(userName: String , password:String ) :LiveData<User>
 
-    @Query("SELECT * FROM USERTABLE")
+    @Query("SELECT * FROM USERTABLE ")
     fun getAllUsers( ) :LiveData<List<User>>
 
 
