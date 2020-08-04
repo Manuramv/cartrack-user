fe2329fa6328de24c0f71579861df8ca6ee516ce
Manuv <manuram692@gmail.com>
2020-08-04 17:48:16 +0800

    splash screen completed

--- app/src/main/java/com/cartrack/users/SplashScreenActivity.kt
+++ app/src/main/java/com/cartrack/users/SplashScreenActivity.kt
@@ -4,22 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.cartrack.users.data.repository.LoginRepositoryFactory
+import com.cartrack.users.databinding.ActivitySplashBinding
 import com.cartrack.users.ui.splash.SplashViewModel
 import com.cartrack.users.ui.ViewModelFactory
 import com.cartrack.users.ui.login.LoginActivity
+import com.cartrack.users.utils.AlertUtils
 import com.cartrack.users.utils.Resource
 
 class SplashScreenActivity : AppCompatActivity() {
     val TAG = SplashScreenActivity::class.java.canonicalName
     private lateinit var splashViewModel: SplashViewModel
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         splashViewModel = ViewModelProvider(this, ViewModelFactory(LoginRepositoryFactory.createLoginRepository(application),SplashViewModel::class.java ))
             .get(SplashViewModel::class.java)
 
@@ -27,8 +31,6 @@ class SplashScreenActivity : AppCompatActivity() {
         splashViewModel.readUserJsonandInsertToDb(this)
         //setup the observer
         setUpObserver()
-
-
     }
 
     //observe for the view model changes
@@ -37,9 +39,14 @@ class SplashScreenActivity : AppCompatActivity() {
             Log.d("tag","insert splas::"+it)
             when(it){
                 true -> navigateToLoginScreen()
-                false -> "show error"
+                false -> showError()
             }
         })
+    }
+
+    //show this error if we are not abel to config the db
+    private fun showError(){
+        AlertUtils.showAlert(this,getString(R.string.splash_error),getString(R.string.db_init_erro) )
     }
 
     private fun navigateToLoginScreen(){
